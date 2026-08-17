2b47c6e76671e17ebdd81da1ccbd6498f0a23428
arielfaqih <arielhf00@gmail.com>
2026-08-17 22:26:56 +0700

    keren

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
   test: {
     environment: 'jsdom',
     include: ['src/**/*.test.{js,jsx}'],
